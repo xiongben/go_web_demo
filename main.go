ca0b3a43582a01d651cea56d15ffe799f5a29af3
Xiong Ben <xiongben1993@gmail.com>
2020-04-12 12:18:01 +0800

    [add] request

--- main.go
+++ main.go
@@ -11,5 +11,19 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "测试http协议")
+	fmt.Fprintln(w, "测试http协议", r.URL.Path)
+	fmt.Fprintln(w, "测试http协议", r.URL.RawQuery)
+	fmt.Fprintln(w, "请求头中的信息", r.Header.Get("Accept-Encoding"))
+	fmt.Fprintln(w, "请求头中的信息", r.Header["Accept-Encoding"])
+	fmt.Fprintln(w, "请求头中的信息", r.Header)
+
+	//len := r.ContentLength
+	//body := make([]byte, len)
+	//r.Body.Read(body)
+	//fmt.Fprintln(w,"请求体中的内容有:", string(body))
+
+	pass := r.PostFormValue("pass")
+	fmt.Fprintln(w, "pass: ", pass)
+	//username := r.PostFormValue("username")
+	//fmt.Fprintln(w,"username: ", username)
 }
