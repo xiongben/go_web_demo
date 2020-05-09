f6a81a6e5222e4179f14f9d414c80e4cad80d5c3
Xiong Ben <xiongben1993@gmail.com>
2020-05-09 20:44:18 +0800

    [add] chatroom

--- chatdemo/server/model/userDao.go
+++ chatdemo/server/model/userDao.go
@@ -17,15 +17,34 @@ func NewUserDao(pool *sql.DB) (userDao *UserDao) {
 }
 
 func (this *UserDao) GetUserById(id int) (user UserSql, err error) {
-	rows, err := this.pool.Query("SELECT * FROM user where id = ?", id)
+	var name string
+	var password string
+	err = this.pool.QueryRow("SELECT username,password FROM user where id = ?", id).Scan(&name, &password)
+	if err == sql.ErrNoRows {
+		fmt.Println("there is no user's id is XXXX")
+		err = ERROR_USER_NOTEXISTS
+		return
+	} else {
+		user.Username = name
+		user.Password = password
+		fmt.Println(user)
+	}
+	//for rows.Next() {
+	//	var user = UserSql{}
+	//	rows.Scan(&user.Id, &user.Username, &user.Birthday, &user.Sex, &user.Address, &user.Password)
+	//	fmt.Println(user)
+	//}
+	return
+}
+
+func (this *UserDao) Login(userid int, userPwd string) (user UserSql, err error) {
+	user, err = this.GetUserById(userid)
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
-	for rows.Next() {
-		var user = UserSql{}
-		rows.Scan(&user.Id, &user.Username, &user.Birthday, &user.Sex, &user.Address, &user.Password)
-		fmt.Println(user)
+	if user.Password != userPwd {
+		err = ERROR_USER_PWD
+		return
 	}
 	return
 }
--- chatdemo/server/process/userProcess.go
+++ chatdemo/server/process/userProcess.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"go_web_demo/chatdemo/common/message"
+	"go_web_demo/chatdemo/server/model"
 	"go_web_demo/chatdemo/server/utils"
 	"net"
 )
@@ -13,7 +14,7 @@ type UserProcess struct {
 	UserId int
 }
 
-func (this *UserProcess) ServerProcessLogin(mes *message.Message) (err error) {
+func (this *UserProcess) ServerProcessLogin(mes *message.Message) (user model.UserSql, err error) {
 	var loginMes message.LoginMes
 	err = json.Unmarshal([]byte(mes.Data), &loginMes)
 	if err != nil {
@@ -24,7 +25,22 @@ func (this *UserProcess) ServerProcessLogin(mes *message.Message) (err error) {
 	resMes.Type = message.LoginResMesType
 	var loginResMes message.LoginResMes
 	//数据库操作
-	loginResMes.Code = 200
+	user, err = model.MyUserDao.Login(loginMes.UserId, loginMes.UserPwd)
+	if err != nil {
+		if err == model.ERROR_USER_NOTEXISTS {
+			loginResMes.Code = 500
+			loginResMes.Error = err.Error()
+		} else if err == model.ERROR_USER_PWD {
+			loginResMes.Code = 403
+			loginResMes.Error = err.Error()
+		} else {
+			loginResMes.Code = 505
+			loginResMes.Error = "server error ..."
+		}
+	} else {
+		loginResMes.Code = 200
+	}
+
 	fmt.Println("user login success!")
 	data, err := json.Marshal(loginResMes)
 	if err != nil {
